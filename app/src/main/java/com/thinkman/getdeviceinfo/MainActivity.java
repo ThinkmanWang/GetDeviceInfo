6e316cd583c878580fb24a9a93ffb1c637a31786
Thinkman Wang <wangxf1985@gmail.com>
2018-11-15 16:59:48 +0800

    update

--- app/src/main/java/com/thinkman/getdeviceinfo/MainActivity.java
+++ app/src/main/java/com/thinkman/getdeviceinfo/MainActivity.java
@@ -2,9 +2,14 @@ package com.thinkman.getdeviceinfo;
 
 import android.Manifest;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -14,13 +19,20 @@ import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    public static final String LOG_TAG = "GET_DEVICEINFO";
 
     @BindView(R.id.tv_imei)
     TextView m_tvImei = null;
 
     @BindView(R.id.tv_imsi)
     TextView m_tvImsi = null;
+
+    @BindView(R.id.tv_step_counter)
+    TextView m_tvStepCounter = null;
+
+    private SensorManager mSensorManager;// 传感器服务
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         MainActivityPermissionsDispatcher.initUIWithCheck(this);
+        initSensor();
     }
 
     @OnClick(R.id.btn_get)
@@ -44,5 +57,99 @@ public class MainActivity extends AppCompatActivity {
         m_tvImsi.setText("imsi: " + tm.getSubscriberId());
     }
 
+    public static int CURRENT_SETP = 0;
+
+    public static float SENSITIVITY = 10;   //SENSITIVITY灵敏度
+
+    private float mLastValues[] = new float[3 * 2];
+    private float mScale[] = new float[2];
+    private float mYOffset;
+    private static long end = 0;
+    private static long start = 0;
+
+    /**
+     * 最后加速度方向
+     */
+    private float mLastDirections[] = new float[3 * 2];
+    private float mLastExtremes[][] = { new float[3 * 2], new float[3 * 2] };
+    private float mLastDiff[] = new float[3 * 2];
+    private int mLastMatch = -1;
+
+    public void initSensor() {
+        int h = 480;
+        mYOffset = h * 0.5f;
+        mScale[0] = -(h * 0.5f * (1.0f / (SensorManager.STANDARD_GRAVITY * 2)));
+        mScale[1] = -(h * 0.5f * (1.0f / (SensorManager.MAGNETIC_FIELD_EARTH_MAX)));
+
+        mSensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
+        // 注册传感器，注册监听器
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Log.i(Constant.STEP_SERVER, "StepDetector");
+        Sensor sensor = event.sensor;
+        // Log.i(Constant.STEP_DETECTOR, "onSensorChanged");
+        if (sensor.getType() == Sensor.TYPE_ORIENTATION) {
+        } else {
+            int j = (sensor.getType() == Sensor.TYPE_ACCELEROMETER) ? 1 : 0;
+            if (j == 1) {
+                float vSum = 0;
+                for (int i = 0; i < 3; i++) {
+                    final float v = mYOffset + event.values[i] * mScale[j];
+                    vSum += v;
+                }
+                int k = 0;
+                float v = vSum / 3;
+
+                float direction = (v > mLastValues[k] ? 1: (v < mLastValues[k] ? -1 : 0));
+                if (direction == -mLastDirections[k]) {
+                    // Direction changed
+                    int extType = (direction > 0 ? 0 : 1); // minumum or
+                    // maximum?
+                    mLastExtremes[extType][k] = mLastValues[k];
+                    float diff = Math.abs(mLastExtremes[extType][k]- mLastExtremes[1 - extType][k]);
+
+                    if (diff > SENSITIVITY) {
+                        boolean isAlmostAsLargeAsPrevious = diff > (mLastDiff[k] * 2 / 3);
+                        boolean isPreviousLargeEnough = mLastDiff[k] > (diff / 3);
+                        boolean isNotContra = (mLastMatch != 1 - extType);
+
+                        if (isAlmostAsLargeAsPrevious && isPreviousLargeEnough && isNotContra) {
+                            end = System.currentTimeMillis();
+                            if (end - start > 500) {// 此时判断为走了一步
+                                Log.i(LOG_TAG, "CURRENT_SETP:"
+                                        + CURRENT_SETP);
+                                CURRENT_SETP++;
+                                mLastMatch = extType;
+                                start = end;
+
+                                MainActivity.this.runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        m_tvStepCounter.setText(String.format("%d", CURRENT_SETP));
+                                    }
+                                });
+                            }
+                        } else {
+                            mLastMatch = -1;
+                        }
+                    }
+                    mLastDiff[k] = diff;
+                }
+                mLastDirections[k] = direction;
+                mLastValues[k] = v;
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
 
 }
